f5426aaf4d52a2d61caaacb525f2272aec3523ea
Robert Ancell <robert.ancell@gmail.com>
2025-11-01 23:26:31 +0800

    Add missing example

---
+++ examples/socket_server.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "ipv4_socket.h"
+
+int main(int argc, char **argv) {
+  uint16_t port = 1234;
+  IPv4Socket *socket = ipv4_socket_new_tcp();
+  IPv4Address *address = ipv4_address_new_any();
+  ipv4_socket_bind(socket, address, port);
+  ipv4_address_unref(address);
+
+  ipv4_socket_listen(socket);
+  printf("Listening on port %d\n", port);
+
+  while (true) {
+    IPv4Socket *client = ipv4_socket_accept(socket);
+    printf("Got connection!\n");
+    const char *message = "Hello world!";
+    ipv4_socket_send(client, (const uint8_t *)message, strlen(message) + 1);
+    ipv4_socket_unref(client);
+  }
+
+  return 0;
+}
